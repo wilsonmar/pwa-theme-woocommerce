90e53b4ae22740c02ef840b1c717a58e455a7e90
Wilson Mar <WilsonMar+GitHub@gmail.com>
2018-08-10 14:53:04 -0600

    add heroku

---
+++ google-code.js
@@ -0,0 +1,26 @@
+google-code.js
+
+# From https://cloud.google.com/nodejs/
+
+// Imports the Google Cloud client library
+const Storage = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+// const bucketName = 'Name of a bucket, e.g. my-bucket';
+// const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
+
+// Uploads a local file to the bucket
+storage
+  .bucket(bucketName)
+  .upload(filename)
+  .then(() => {
+    console.log(`${filename} uploaded to ${bucketName}.`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
--- install.sh
+++ install.sh
@@ -6,3 +6,39 @@ cd pwa-theme-woocommerce
 npm install
 npm start
    # auto open http://localhost:3000/
+
+#### Heroku
+
+# https://dashboard.heroku.com/apps
+# New, Create New App, "pwa-react-node", Create App
+# NOT Connect to GitHub. Authorize Heroku. 
+
+heroku login
+# Create a new Git repository
+#Initialize a git repository in a new or existing directory
+
+heroku git:remote -a pwa-react-node
+# Deploy your application
+# Commit your code to the repository and deploy it to Heroku using Git.
+
+git add .
+git commit -am "make it better"
+git push heroku master
+# Existing Git repository
+# For existing repositories, simply add the heroku remote
+
+heroku git:remote -a pwa-react-node
+
+
+#### GCP
+
+# https://cloud.google.com/nodejs/
+npm install --save @google-cloud/storage 
+
+# GCP Project: jetbloom-pwareact-node
+   # Assigned: starting-account-2sn1bf9lqx89.
+   # Downlaod private key: jetbloom-pwareact-node-9c6c833f9aec.json in ~
+
+# Sample: https://github.com/googleapis/nodejs-storage/blob/fb6a7fd779061da9d885b7ac89992acde1191b54/samples/files.js
+
+# Needs app.yml for Google
